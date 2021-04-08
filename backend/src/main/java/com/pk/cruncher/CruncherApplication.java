d4ce1c04cb72d851852ff7060d2a0b761842c55f
polina-krukovich <polina.krukovich7@gmail.com>
2021-04-09 01:35:13 +0300

    initial commit

---
+++ backend/src/main/java/com/pk/cruncher/CruncherApplication.java
@@ -0,0 +1,13 @@
+package com.pk.cruncher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruncherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruncherApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pk/cruncher/CruncherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pk.cruncher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruncherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
